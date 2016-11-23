4c75ace59448d30c966ccc6b0ba76dbbe9a782e6
Roberto B <robertbg08@gmail.com>
2016-11-23 17:38:40 +0100

    tuberias

--- 11_pipes/pipes_txe.cpp
+++ 11_pipes/pipes_txe.cpp
@@ -12,6 +12,7 @@
 
 void writer(const char *mssg, int times, FILE *stream_out){
     
+   
     const char *letter;
     
     for(letter=mssg; times>0; --times, letter=mssg)
@@ -19,6 +20,7 @@ void writer(const char *mssg, int times, FILE *stream_out){
             fputc(*letter, stream_out);
             fputc(*letter, stderr);
             fflush(stream_out);
+            letter++;
             usleep(100000);
         }
     
@@ -33,29 +35,54 @@ void reader(FILE *stream_in){
         fputc('\a', stdout);
         fflush(stdout);
     }
+    
 }
 
 int main(int argc, char *argv[]){
     
     int fds[2];
-   /*
+    pid_t pid;
+    
     if(pipe(fds) == -1){
         fprintf(stderr, "pipe invoked FAILED..\n");
         return EXIT_FAILURE;
     }
-  */
-    pipe(fds);
-    pid_t child_pid = fork();
+
     
-    if (child_pid) {
+    pid = fork();
+    /*
+    if(pid == (pid_t) 0){
+    
+        close(fds[1]);
+        reader(fdopen(fds[0], "r"));
         close(fds[0]);
-        writer("The World is a Vampire.\n", 5, fdopen(fds[1], "w"));
+    }else{
+        close(fds[0]);
+        writer("World is a vampire\n", 5,fdopen(fds[1], "w"));
+        close(fds[1]);
+    }
+   */
+    
+   
+    if(pipe(fds) == -1){
+        fprintf(stderr, "pipe invoked FAILED..\n");
+        return EXIT_FAILURE;
+    }
+
+    
+    pid = fork();
+    //if(!pid){
+    if (pid == (pid_t) 0) {
+        close(fds[0]);
+        writer("World is a Vampire\n", 5, fdopen(fds[1], "w"));
         close(fds[1]);
     } else {
         close(fds[1]);
         reader(fdopen(fds[0], "r"));
         close(fds[0]);
     }
+    
+    
     return EXIT_SUCCESS;
 }
 
